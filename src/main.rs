3bd598f5ad76ef44517b0c873cb70518d3502cfc
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-05-02 16:13:03 +0900

    【add】数当てゲームのユーザーの入力を受け取る箇所を実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        // memo 参照もデフォルトで不変なのでmutを付ける必要がある
+        .read_line(&mut guess)
+        // expectを書かないと警告が出る
+        // エラーの場合にクラッシュさせて、引数のメッセージを表示する
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
