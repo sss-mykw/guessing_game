c0901394a0e8a95d3c54e8f0c2376cbb0c0667cf
sss-mykw <64953743+sss-mykw@users.noreply.github.com>
2025-05-02 16:44:58 +0900

    【add】予想と秘密の数字を比較する

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use std::cmp::Ordering;
 use std::io;
 use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    
+
     // 1から100までの乱数
     // 下限値は含まれるが上限値は含まれないことに注意
     let secret_number = rand::rng().random_range(1..101);
@@ -23,5 +24,18 @@ fn main() {
         // エラーの場合にクラッシュさせて、引数のメッセージを表示する
         .expect("Failed to read line");
 
+    let guess: u32 = guess
+        // 空白や改行文字を削除
+        .trim()
+        // 変数宣言時に型を明示しておくことで、その型に変換してくれる
+        .parse()
+        .expect("Please type a number!");
+    
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => { println!("Too small!") }
+        Ordering::Greater => { println!("Too big!") }
+        Ordering::Equal => { println!("You win!") }
+    }
 }
